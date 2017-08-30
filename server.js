dc52a163d24af0486e0524b6e8bd46224144b05c
Josh Kirby <jakirby@live.ca>
2017-08-30 21:02:24 +0000

    Inital commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view-engine", "hbs");
+// Log date time and url to server.log file
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile("server.log",log + "\n", (err) => {
+           if(err) {
+               console.log(err);
+           } 
+        });
+    next();
+});
+// Enable maintance mode
+// app.use((req, res, next) => {
+//   res.render("maintance.hbs"); 
+// });
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper("screamIt", (text) => {
+    return text.toUpperCase();
+});
+
+app.get("/",(req, res) => {
+    res.render("home.hbs", {
+       pageTitle: "Home Page",
+       welcomeMessage: "Welcome to my site!"
+   });
+});
+
+app.get("/about", (req, res) => {
+   res.render("about.hbs", {
+       pageTitle: "About Page",
+   });
+});
+
+app.get("/bad", (req, res) => {
+    res.send({
+        errorMessage: "Unable to handle request"
+    });   
+});
+
+app.listen(process.env.PORT, process.env.IP, () => {
+    console.log("The Express Server Has Started!");
+});
